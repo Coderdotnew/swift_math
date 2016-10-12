91296c66ef46b5b676a29f4d06380b1d3a736483
Stephanholly <sholly@usc.edu>
2016-10-12 11:09:41 -0700

    first

---
+++ swift_math.swift
@@ -0,0 +1,14 @@
+// re-code the program below such that every line prints true to the screen
+
+
+print(100 % 25 == 5)
+print(1.5 * 2 == 3.1)
+print("swift" == "swift ")
+print("Stephen Hawking" == "stephen" + "hawking")
+print("SWIFT RULES!!!" == "swift rules!!!") //use a method to make this true
+print(pow(3,2) != 9)
+print(44 % 11 == 4)
+print(20 % 20 != 0)
+print("Star Wars" != "Star " + "Wars")
+print(("5" + "5" == "55") && ("swift" == "Swift" || 9 > 9))
+print((("5" + "5" == "55") && ("swift" == "Swift")))
